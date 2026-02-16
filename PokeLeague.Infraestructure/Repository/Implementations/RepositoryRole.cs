94cc3fa421fc38588200b01fd1117aa782b5770b
Andrés Bolaños <andresbol1706@gmail.com>
2026-02-16 00:30:21 -0600

    AddRepositoryRole

---
+++ PokeLeague.Infraestructure/Repository/Implementations/RepositoryRole.cs
@@ -0,0 +1,46 @@
+﻿using PokeLeague.Infraestructure.Data;
+using PokeLeague.Infraestructure.Models;
+using PokeLeague.Infraestructure.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeLeague.Infraestructure.Repository.Implementations
+{
+    public class RepositoryRole : IRepositoryRole
+    {
+        private readonly PokeLeagueContext _context;
+
+        public RepositoryRole(PokeLeagueContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> AddAsync(Role role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Role>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Role role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PokeLeague.Infraestructure/Repository/Interfaces/IRepositoryRole.cs
@@ -0,0 +1,18 @@
+﻿using PokeLeague.Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeLeague.Infraestructure.Repository.Interfaces
+{
+    public interface IRepositoryRole
+    {
+        Task<ICollection<Role>> ListAsync();
+        Task<Role> FindByIdAsync(int id);
+        Task<int> AddAsync(Role role);
+        Task<int> UpdateAsync(Role role);
+        Task<int> DeleteAsync(int id); 
+    }
+}

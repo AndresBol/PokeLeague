957b330f38d113bce4034cbc6bb45ce680de3b9a
josue <josue@DESKTOP-TAOQC4P>
2026-02-21 17:22:29 -0600

    Interface & Implementation

---
+++ PokeLeague.Application/Services/Implementations/ServiceRarity.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using PokeLeague.Application.DTOs;
+using PokeLeague.Application.Services.Interfaces;
+using PokeLeague.Infraestructure.Models;
+using PokeLeague.Infraestructure.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeLeague.Application.Services.Implementations
+{
+    public class ServiceRarity : IServiceRarity
+    {
+        private readonly IRepositoryRarity _repository;
+        private readonly IMapper _mapper;
+
+        public ServiceRarity(IRepositoryRarity repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<string> AddAsync(RarityDTO rarityDto)
+        {
+            var rarity = _mapper.Map<Rarity>(rarityDto);
+            var id = await _repository.AddAsync(rarity);
+
+            return id;
+        }
+
+        public async Task<RarityDTO> FindByIdAsync(string id)
+        {
+            var rarity = await _repository.FindByIdAsync(id);
+            var rarityDTO = _mapper.Map<RarityDTO>(rarity);
+
+            return rarityDTO;
+        }
+
+        public async Task<ICollection<RarityDTO>> ListAsync()
+        {
+            var raritys = await _repository.ListAsync();
+            var collection = _mapper.Map<ICollection<RarityDTO>>(raritys);
+
+            return collection;
+        }
+
+        public async Task UpdateAsync(RarityDTO rarityDto)
+        {
+            var rarity = _mapper.Map<Rarity>(rarityDto);
+            rarity.Id = rarityDto.Id;
+            await _repository.UpdateAsync(rarity);
+        }
+        public async Task DeleteAsync(string id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
---
+++ PokeLeague.Application/Services/Interfaces/IServiceRarity.cs
@@ -0,0 +1,18 @@
+﻿using PokeLeague.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeLeague.Application.Services.Interfaces
+{
+    public interface IServiceRarity
+    {
+        Task<ICollection<RarityDTO>> ListAsync();
+        Task<RarityDTO> FindByIdAsync(string id);
+        Task<string> AddAsync(RarityDTO rarityDto);
+        Task UpdateAsync(RarityDTO rarityDto);
+        Task DeleteAsync(string id);
+    }
+}

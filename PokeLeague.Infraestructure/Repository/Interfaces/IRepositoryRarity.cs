0ebe8cd0677b42e71c731d30822438a8cdb65c05
josue <josue@DESKTOP-TAOQC4P>
2026-02-21 16:34:27 -0600

    Inteface repository rarity

---
+++ PokeLeague.Infraestructure/Repository/Interfaces/IRepositoryRarity.cs
@@ -0,0 +1,18 @@
+﻿using PokeLeague.Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeLeague.Infraestructure.Repository.Interfaces
+{
+    public interface IRepositoryRarity
+    {
+        Task<ICollection<Rarity>> ListAsync();
+        Task<Rarity> FindByIdAsync(string id);
+        Task<string> AddAsync(Rarity rarity);
+        Task UpdateAsync(Rarity rarity);
+        Task DeleteAsync(string id); 
+    }
+}

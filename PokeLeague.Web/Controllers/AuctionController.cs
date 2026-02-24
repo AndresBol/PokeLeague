f729b6bc7a38901408fa0f1de47b34d5c8eca609
josue <josue@DESKTOP-TAOQC4P>
2026-02-23 20:15:31 -0600

    Index View

---
+++ PokeLeague.Web/Controllers/AuctionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeLeague.Application.Services.Interfaces;
+
+namespace PokeLeague.Web.Controllers
+{
+    public class AuctionController : Controller
+    {
+        private readonly IServiceAuction _serviceAuction;
+
+        public AuctionController(IServiceAuction serviceAuction)
+        {
+            _serviceAuction = serviceAuction;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var autions = await _serviceAuction.ListAsync();
+
+            return View(autions);
+        }
+    }
+}

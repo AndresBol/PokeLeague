81756b1d9ec74ff19927e81d868861e6395d2ca0
Andres <andresbol1706@gmail.com>
2026-04-19 06:10:22 -0600

    Add PurchaseOrder service with user filtering

--- PokeLeague.Application/Services/Interfaces/IServicePurchaseOrder.cs
+++ PokeLeague.Application/Services/Interfaces/IServicePurchaseOrder.cs
@@ -10,9 +10,13 @@ namespace PokeLeague.Application.Services.Interfaces
     public interface IServicePurchaseOrder
     {
         Task<ICollection<PurchaseOrderDTO>> ListAsync();
+        Task<ICollection<PurchaseOrderDTO>> ListByUserIdAsync(int userId);
         Task<PurchaseOrderDTO> FindByIdAsync(int id);
+        Task<PurchaseOrderDTO?> FindByAuctionIdAsync(int auctionId);
         Task<int> AddAsync(PurchaseOrderDTO purchaseOrderDto);
         Task UpdateAsync(PurchaseOrderDTO purchaseOrderDto);
         Task DeleteAsync(int id);
+        Task<int> RegisterPaymentForAuctionAsync(int auctionId);
+        Task ConfirmPaymentAsync(int id);
     }
 }
